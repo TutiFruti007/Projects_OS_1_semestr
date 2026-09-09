d7a230ba831c5b1ecc2f2f1aaaeb212e6574030e
Grisha <vasysamsung70@gmail.com>
2026-09-09 08:49:11 +0700

    Реализация Finance и Program

---
+++ FinanceApp/Finance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp
+{
+    internal class Finance
+    {
+        public DateTime Date { get; set; }
+        public string Resource { get; set; }
+        public int Sum { get; set; }
+        public void FromString(string input) 
+        { 
+            string[] s = input.Trim().Split(' '); 
+            Date = DateTime.Parse(s[0]); 
+            Resource = s[1]; 
+            Sum = Convert.ToInt32(s[2]); 
+        }
+        public Finance() { }
+    }
+}
--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FinanceApp
 {
@@ -10,6 +11,12 @@ namespace FinanceApp
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine(); 
+            Finance finance = new Finance(); 
+            finance.FromString(input); 
+            string path = "data.txt"; 
+            string content = $"{finance.Date}, {finance.Resource}, {finance.Sum}"; 
+            File.WriteAllText(path, content);
         }
     }
 }
