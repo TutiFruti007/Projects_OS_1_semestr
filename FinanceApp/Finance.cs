c132b479f74456969adda13982f274111f178e30
Grisha <vasysamsung70@gmail.com>
2026-09-10 15:38:13 +0700

    Добавление плюс дня, искать минимальный день, среднюю дату

--- FinanceApp/Finance.cs
+++ FinanceApp/Finance.cs
@@ -18,6 +18,56 @@ namespace FinanceApp
             Resource = s[1]; 
             Sum = Convert.ToInt32(s[2]); 
         }
+        public void MinDate(Finance[] finance)
+        {
+            Finance min = finance[0];
+            foreach (Finance item in finance)
+            {
+                if (item.Date < min.Date)
+                {
+                    min = item;
+                }
+            }
+
+        }
+        public void PlusDay()
+        {
+            this.Date = this.Date.AddDays(1);
+        }
+        public Finance BetweenDays(Finance[] finance)
+        {
+            Finance min = finance[0];
+            Finance max = finance[0];
+
+            foreach (Finance item in finance)
+            {
+                if (item.Date < min.Date)
+                {
+                    min = item;
+                }
+
+                if (item.Date > max.Date)
+                {
+                    max = item;
+                }
+            }
+
+            DateTime middle = min.Date.AddDays((max.Date - min.Date).Days / 2);
+
+            Finance close = finance[0];
+
+            foreach (Finance item in finance)
+            {
+                if (Math.Abs((item.Date - middle).Days) <
+                    Math.Abs((close.Date - middle).Days))
+                {
+                    close = item;
+                }
+            }
+
+            return close;
+        }
+
         public Finance() { }
     }
 }
--- FinanceApp/Program.cs
+++ FinanceApp/Program.cs
@@ -11,11 +11,30 @@ namespace FinanceApp
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine(); 
-            Finance finance = new Finance(); 
-            finance.FromString(input); 
-            string path = "data.txt"; 
-            string content = $"{finance.Date}, {finance.Resource}, {finance.Sum}"; 
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            string input4 = Console.ReadLine();
+
+            Finance finance = new Finance();
+            Finance finance2 = new Finance();
+            Finance finance3 = new Finance();
+            Finance finance4 = new Finance();
+
+            finance.FromString(input);
+            finance2.FromString(input2);
+
+            finance.MinDate(new Finance[] { finance, finance2 });
+
+            finance.PlusDay();
+
+            Finance days = new Finance().BetweenDays(new Finance[] { finance, finance2, finance3, finance4 });
+
+            string path = "data.txt";
+
+            string content = $"{finance.Date}, {finance.Resource}, {finance.Sum}" +
+                $" \n {finance2.Date}, {finance2.Resource}, {finance2.Sum} " +
+                $"\n {days} ";
             File.WriteAllText(path, content);
         }
     }
